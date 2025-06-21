abae6a11da1f37219b45345344f46e095f05da77
padel77 <adelassem99@gmail.com>
2025-06-21 17:49:13 +0300

    new update
    new update

--- src/components/home/PropertyCard.tsx
+++ src/components/home/PropertyCard.tsx
@@ -3,18 +3,32 @@ import Image from "next/image";
 import React, { useState } from "react";
 import { Timeline } from "@/components/ui/timeline";
 import { useTranslations } from "next-intl";
-import logo from "@../../../public/assets/package/content management/1.png";
-import logo2 from "@../../../public/assets/package/content management/2.png";
-import logo3 from "@../../../public/assets/package/content management/3.png";
-import logo4 from "@../../../public/assets/package/content management/1.png";
-import logo5 from "@../../../public/assets/package/social media/1.png";
-import logo6 from "@../../../public/assets/package/social media/2.png";
-import logo7 from "@../../../public/assets/package/social media/3.png";
+import logo from "@../../../public/assets/package/content management/1.jpeg";
+import logo2 from "@../../../public/assets/package/content management/2.jpeg";
+import logo3 from "@../../../public/assets/package/content management/3.jpeg";
+import logo4 from "@../../../public/assets/package/content management/7.jpeg";
+import logo56 from "@../../../public/assets/package/content management/5.jpeg";
+import logo65 from "@../../../public/assets/package/content management/6.jpeg";
+import logo5 from "@../../../public/assets/package/social media/1.jpeg";
+import logo6 from "@../../../public/assets/package/social media/2.jpeg";
+import logo7 from "@../../../public/assets/package/social media/3.jpeg";
+import logo71 from "@../../../public/assets/package/social media/4.jpeg";
+import logo72 from "@../../../public/assets/package/social media/5.jpeg";
+import logo73 from "@../../../public/assets/package/social media/6.jpeg";
 import logo8 from "@../../../public/assets/Used equipment.png";
-import logo9 from "@../../../public/assets/package/photograph/1.png";
-import logo10 from "@../../../public/assets/package/photograph/2.png";
-import logo11 from "@../../../public/assets/package/photograph/3.png";
-import logo12 from "@../../../public/assets/package/photograph/1.png";
+import logo9 from "@../../../public/assets/package/program/1.jpeg";
+import logo10 from "@../../../public/assets/package/program/2.jpeg";
+import logo11 from "@../../../public/assets/package/program/7.jpeg";
+import logo12 from "@../../../public/assets/package/program/8.jpeg";
+import logo13 from "@../../../public/assets/package/program/5.jpeg";
+import logo14 from "@../../../public/assets/package/program/6.jpeg";
+import logo15 from "@../../../public/assets/package/photograph/1.jpeg";
+import logo16 from "@../../../public/assets/package/photograph/2.jpeg";
+import logo17 from "@../../../public/assets/package/photograph/3.jpeg";
+import logo18 from "@../../../public/assets/package/photograph/4.jpeg";
+import logo19 from "@../../../public/assets/package/photograph/5.jpeg";
+import logo20 from "@../../../public/assets/package/photograph/6.jpeg";
+
 export function TimelineDemo() {
   const t = useTranslations("about");
   const [previewImage, setPreviewImage] = useState(null as string | null);
@@ -30,7 +44,7 @@ export function TimelineDemo() {
           </p>
 
             <div className="grid grid-cols-2 gap-4">
-            {[logo, logo2, logo3, logo4].map((imageSrc, index) => (
+            {[logo, logo2, logo3, logo4,logo56,logo65].map((imageSrc, index) => (
               <div key={index} className="relative">
               <Image
                 src={imageSrc}
@@ -72,7 +86,57 @@ export function TimelineDemo() {
       ),
     },
     {
-      id : 2,
+      id: 2 ,
+      title: t("photographic"),
+      content: (
+        <div>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-bold  mb-8">
+            {t("photographicContent")}{" "}
+          </p>
+            <div className="grid grid-cols-2 gap-4">
+            {[logo15, logo16, logo17, logo18,logo19,logo20].map((imageSrc, index) => (
+              <div key={index} className="relative">
+              <Image
+                src={imageSrc}
+                alt={`startup template ${index + 1}`}
+                width={500}
+                height={500}
+                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                onClick={() => setPreviewImage(imageSrc?.src)}
+              />
+              </div>
+            ))}
+            {previewImage && (
+              <div
+              className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+              >
+              <div className="relative">
+                <Image
+                onClick={() => setPreviewImage(null)}
+                src={previewImage}
+                alt="Preview"
+                width={600}
+                height={600}
+                className="rounded-lg object-cover"
+                />
+                <button
+                className="absolute top-2 right-2 bg-white rounded-full p-2 shadow-md"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setPreviewImage(null);
+                }}
+                >
+                ✕
+                </button>
+              </div>
+              </div>
+            )}
+            </div>
+        </div>
+      ),
+    },
+    {
+      id : 3,
       title: t("earlyYearTitle"),
       content: (
         <div>
@@ -81,7 +145,7 @@ export function TimelineDemo() {
           </p>
 
           <div className="grid grid-cols-2 gap-4">
-          {[logo5, logo6, logo7].map((imageSrc, index) => (
+          {[logo5, logo6, logo7, logo71,logo72,logo73].map((imageSrc, index) => (
               <div key={index} className="relative">
               <Image
                 src={imageSrc}
@@ -123,13 +187,13 @@ export function TimelineDemo() {
       ),
     },
     {
-      id : 3,
+      id : 4,
       title: t("Changelog"),
       content: (
         <div>
           {/* <p className="text-neutral-800 dark:text-neutral-200 text-md md:text-sm font-bold mb-4">
             {t("morelastWork")}
-          </p>
+          </p> */}
           <div className="mb-8">
             <div className="flex gap-2 font-bold items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
               ✅ {t("morelastWorkcontent")}
@@ -146,9 +210,9 @@ export function TimelineDemo() {
             <div className="flex gap-2 font-bold items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
               ✅ {t("morelastWorkcontent5")}
             </div>
-          </div> */}
+          </div>
           <div className="grid grid-cols-2 gap-4">
-          {[logo9, logo10, logo11, logo12].map((imageSrc, index) => (
+          {[logo9, logo10, logo11, logo12,logo13,logo14].map((imageSrc, index) => (
               <div key={index} className="relative">
               <Image
                 src={imageSrc}
@@ -189,6 +253,7 @@ export function TimelineDemo() {
         </div>
       ),
     },
+    
   ];
   return (
     <div id="about" className="w-full">
--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -35,7 +35,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   const { scrollYProgress } = useScroll({
     target: containerRef,
-    offset: ["start 10%", "end 50%"],
+    offset: ["start 10%", "end 80%"],
   });
 
   const heightTransform = useTransform(scrollYProgress, [0, 1], [0, height]);
@@ -43,18 +43,11 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div
-      className="w-full  bg-white dark:bg-neutral-950 font-sans md:px-10"
+      className="w-full  bg-white dark:bg-neutral-950 font-sans "
       ref={containerRef}
     >
-      <div className="md:flex items-center justify-around space-y-4 max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <div className="flex flex-col center space-y-4">
-        <h2 className="text-2xl md:text-5xl mb-4 text-black font-black dark:text-white max-w-4xl">
-          {t("aboutUs")}
-        </h2>
-        <p className="text-neutral-900 dark:text-neutral-300 text-md md:text-base font-bold max-w-md">
-          {t("aboutElmohandas")}
-        </p>
-        </div>
+      <div className="md:flex items-center justify-around space-y-4 max-w-7xl mx-auto py-10 px-4 md:px-8 lg:px-10">
+
         {/* <div className="flex items-center py-2 gap-4">
           <div>
             <Link
@@ -77,14 +70,26 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           </div>
         </div> */}
         <Image
-              src={logo12}
-              alt="cards template"
-              width={300}
-              height={250}
-              className="rounded-lg md:w-auto w-full  shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
+          src={logo12}
+          alt="cards template"
+          width={300}
+          height={250}
+          className="rounded-lg md:w-auto w-full  shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+        />
+        <div className="flex flex-col center space-y-4">
+          <h2 className="text-2xl md:text-5xl mb-4 text-black font-black dark:text-white max-w-4xl">
+            {t("aboutUs")}
+          </h2>
+          <p className="text-neutral-900 dark:text-neutral-300 text-md md:text-base font-bold max-w-md">
+            {t("aboutElmohandas")}
+          </p>
+        </div>
       </div>
-
+      <div className="col-span-3 text-center mt-4 bg-[#140D2E] text-white dark:text-white dark:bg-neutral-950 p-4 rounded-lg shadow-md">
+        <h1 className="text-2xl  md:text-4xl font-black">
+          {t("portfolio")}
+        </h1>
+      </div>
       <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
         {data.map((item) => (
           <div
@@ -93,32 +98,28 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
               <div
-                className={`h-10 absolute ${
-                  pathname === "/ar"
-                    ? "right-3 md:right-3"
-                    : "left-3 md:left-3 "
-                }   w-10 rounded-full bg-white dark:bg-black flex items-center justify-center`}
+                className={`h-10 absolute ${pathname === "/ar"
+                  ? "right-3 md:right-3"
+                  : "left-3 md:left-3 "
+                  }   w-10 rounded-full bg-white dark:bg-black flex items-center justify-center`}
               >
                 <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
               </div>
               <h3
-                className={`hidden md:block text-xl ${
-                  pathname === "/ar" ? " md:pr-20" : "md:pl-20"
-                } md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 `}
+                className={`hidden md:block text-xl ${pathname === "/ar" ? " md:pr-20" : "md:pl-20"
+                  } md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 `}
               >
                 {item.title}
               </h3>
             </div>
 
             <div
-              className={`relative ${
-                pathname === "/ar" ? "pr-20 pl-4 md:pr-4" : "pl-20 pr-4 md:pl-4"
-              } w-full`}
+              className={`relative ${pathname === "/ar" ? "pr-20 pl-4 md:pr-4" : "pl-20 pr-4 md:pl-4"
+                } w-full`}
             >
               <h3
-                className={`md:hidden block text-2xl mb-4 ${
-                  pathname === "/ar" ? " text-right" : "text-left"
-                }  font-bold text-neutral-500 dark:text-neutral-500`}
+                className={`md:hidden block text-2xl mb-4 ${pathname === "/ar" ? " text-right" : "text-left"
+                  }  font-bold text-neutral-500 dark:text-neutral-500`}
               >
                 {item.title}
               </h3>
@@ -130,9 +131,8 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           style={{
             height: height + "px",
           }}
-          className={`absolute ${
-            pathname === "/ar" ? "md:right-8 right-8" : "md:left-8 left-8"
-          }  top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] `}
+          className={`absolute ${pathname === "/ar" ? "md:right-8 right-8" : "md:left-8 left-8"
+            }  top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] `}
         >
           <motion.div
             style={{
